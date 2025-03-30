7172f641e567415c64ab49423a638652c3f74145
zhunan915 <zhunan@bu.edu>
2025-03-30 14:57:06 -0700

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Create a new VPC using the official module from the Terraform Registry
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.0"
@@ -22,8 +21,6 @@ module "vpc" {
   }
 }
 
-# --- Bastion Host Setup ---
-# Create a security group for the bastion host, allowing SSH only from your IP.
 resource "aws_security_group" "bastion_sg" {
   name   = "bastion-sg"
   vpc_id = module.vpc.vpc_id
@@ -32,7 +29,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.bastion_allowed_ip]   # e.g., "X.X.X.X/32"
+    cidr_blocks = [var.bastion_allowed_ip]
   }
 
   egress {
@@ -43,28 +40,24 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
-# Provision the bastion host in a public subnet.
 resource "aws_instance" "bastion" {
-  ami                         = "ami-01f5a0b78d6089704"  # Amazon Linux 2 AMI for bastion
+  ami                         = "ami-01f5a0b78d6089704"
   instance_type               = "t2.micro"
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   key_name                    = var.key_name
-
-  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
 
   tags = {
     Name = "BastionHost"
+    Role = "bastion"
   }
 }
 
-# --- Private EC2 Instances Setup ---
-# Create a security group for the private EC2 instances.
 resource "aws_security_group" "private_sg" {
   name   = "private-sg"
   vpc_id = module.vpc.vpc_id
 
-  # Allow SSH from the bastion host's security group.
   ingress {
     from_port       = 22
     to_port         = 22
@@ -80,16 +73,39 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
-# Create 6 EC2 instances in private subnets using the custom AMI from Packer.
-resource "aws_instance" "app" {
-  count                  = 6
-  ami                    = "ami-033902bac590411ea"
-  instance_type          = "t2.micro"
-  subnet_id              = element(module.vpc.private_subnets, count.index % length(module.vpc.private_subnets))
-  key_name               = var.key_name
+resource "aws_instance" "ubuntu_app" {
+  count         = 3
+  ami           = "ami-0f9de6e2d2f067fca"
+  instance_type = "t2.micro"
+  subnet_id     = element(module.vpc.private_subnets, count.index)
+  key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.private_sg.id]
-
   tags = {
-    Name = "AppInstance-${count.index + 1}"
+    Name = "UbuntuApp-${count.index + 1}"
+    OS   = "ubuntu"
+  }
+}
+
+resource "aws_instance" "amazon_app" {
+  count         = 3
+  ami           = "ami-01f5a0b78d6089704"
+  instance_type = "t2.micro"
+  subnet_id     = element(module.vpc.private_subnets, count.index)
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  tags = {
+    Name = "AmazonApp-${count.index + 1}"
+    OS   = "amazon"
+  }
+}
+
+resource "aws_instance" "ansible_controller" {
+  ami           = "ami-0f9de6e2d2f067fca"
+  instance_type = "t2.micro"
+  subnet_id     = module.vpc.private_subnets[0]
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  tags = {
+    Name = "AnsibleController"
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,5 +5,17 @@ output "bastion_public_ip" {
 
 output "app_instance_ids" {
   description = "IDs of the 6 private EC2 instances"
-  value       = aws_instance.app.*.id
+  value       = concat(aws_instance.ubuntu_app[*].id, aws_instance.amazon_app[*].id)
+}
+
+output "ubuntu_private_ips" {
+  value = aws_instance.ubuntu_app[*].private_ip
+}
+
+output "amazon_private_ips" {
+  value = aws_instance.amazon_app[*].private_ip
+}
+
+output "ansible_controller_private_ip" {
+  value = aws_instance.ansible_controller.private_ip
 }
